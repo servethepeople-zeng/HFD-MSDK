5e689e883d43b730649d0e2e9cc990d5c731c772
servethepeople-zeng <634013171@qq.com>
2019-11-19 17:21:38 +0800

    Initial commit

---
+++ app/src/main/java/hfd/msdk/sample/MainActivity.java
@@ -0,0 +1,19 @@
+package hfd.msdk.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import hfd.msdk.sample.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow();
+        }
+    }
+}
